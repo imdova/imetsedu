fb6454e2b7151fa65e74ea0274dd17333be46357
imdova <ahmed.habib2030@gmail.com>
2025-02-04 20:59:03 +0200

    3

--- constants/instructor.ts
+++ constants/instructor.ts
@@ -8,7 +8,7 @@ export type InstructorType = {
 export const instructor: InstructorType = {
   name: "Dr. Ahmed Ibrahim",
   title: "HR Director at Alsalam International Hospital",
-  image: "/images/ai",
+  image: "/images/ai.jpg",
   content: "John Doe is a highly experienced instructor with over 10 years of experience in the field of web development. He has a strong passion for teaching and has helped numerous students achieve their goals.",
 };
 
